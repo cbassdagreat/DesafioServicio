a17f982f78bc9859d42e49d8d1820944e0822983
Cbass <claveria.sebastian@gmail.com>
2022-04-24 12:12:36 -0400

    terminado

--- app/src/main/java/cbass/desafioservicio/MainActivity.kt
+++ app/src/main/java/cbass/desafioservicio/MainActivity.kt
@@ -1,10 +1,14 @@
 package cbass.desafioservicio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import cbass.desafioservicio.databinding.ActivityMainBinding
+import cbass.desafioservicio.service.ContadorService
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Handler.Callback {
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,11 +18,26 @@ class MainActivity : AppCompatActivity() {
 
         with(binding)
         {
-            btnIO.setOnClickListener({
-
+            btnIO.setOnClickListener{
+                if(ContadorService.running)
+                {
+                    ContadorService.stopCount(this@MainActivity)
+                    btnIO.text = "START"
                 }
-            )
+                else
+                {
+                    ContadorService.startCount(this@MainActivity, "Iniciado", Handler(mainLooper,this@MainActivity))
+                    btnIO.text = "STOP"
+                }
+            }
+
         }
+    }
+
+    override fun handleMessage(msg: Message): Boolean {
+        val contador = msg.data.getString("Contador")
+        binding.tvCont.text=contador.toString()
+        return true
     }
 
 
